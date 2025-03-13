8265af7555b87ba9362862808b8b3c94898b8a6e
Jangjinhyeok <zero9936@gmail.com>
2025-03-13 14:14:05 +0900

    Player bp, gamemode bp 추가, player anim bp 추가, player anim instance cpp 추가

---
+++ Source/ProjectZam/PlayerAnimInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZAM_API UPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
