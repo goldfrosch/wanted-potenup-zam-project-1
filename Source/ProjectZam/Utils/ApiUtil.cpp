2d9d8dbb30f9bb0a734484e2c0666e0fdf90fc4d
GoldFrosch <goldfrosch@daum.net>
2025-03-13 12:06:07 +0900

    feat: Get, Post 관련 세팅 추가

---
+++ Source/ProjectZam/Utils/ApiUtil.cpp
@@ -0,0 +1,77 @@
+﻿#include "ApiUtil.h"
+
+#include "HttpModule.h"
+#include "JsonObjectConverter.h"
+#include "Interfaces/IHttpRequest.h"
+
+FAPIUtil::FAPIUtil()
+{
+	MainAPI.MainURL = "http://192.168.10.96:8001";
+}
+
+void FAPIUtil::ParsingData(UStruct Params, FString& Result) const
+{
+	FJsonObjectConverter::UStructToJsonObjectString(Params, Result);
+}
+
+void FAPIUtil::GetApi(const FApiRequest& Request, FApiResponse& Response) const
+{
+	if (Response.IsLoading)
+	{
+		return;
+	}
+	
+	const FHttpRequestRef HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetURL(FString::Printf("%ls%ls", *MainURL, *Request.Path));
+	HttpRequest->SetVerb("GET");
+	HttpRequest->SetHeader(TEXT("Content-type"), TEXT("application/json"));
+
+	for (auto NewHeader : Request.Header)
+	{
+		HttpRequest->SetHeader(NewHeader.Key, NewHeader.Value);
+	}
+	
+	Response.IsLoading = true;
+	
+	HttpRequest->OnProcessRequestComplete().BindLambda(
+		[&](const FHttpRequestPtr& Req, const FHttpResponsePtr& Res)
+	{
+		Response.IsLoading = false;
+		Request.Callback(Req, Res);
+	});
+	
+	HttpRequest->ProcessRequest();
+}
+
+void FAPIUtil::PostApi(const FApiRequest& Request, FApiResponse& Response) const
+{
+	if (Response.IsLoading)
+	{
+		return;
+	}
+	
+	const FHttpRequestRef HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetURL(FString::Printf("%ls%ls", *MainURL, *Request.Path));
+	HttpRequest->SetVerb("POST");
+	
+	FString ParamsString;
+	ParsingData(Request.Params, ParamsString);
+	
+	HttpRequest->SetHeader(TEXT("Content-type"), TEXT("application/json"));
+
+	for (auto NewHeader : Request.Header)
+	{
+		HttpRequest->SetHeader(NewHeader.Key, NewHeader.Value);
+	}
+
+	Response.IsLoading = true;
+	
+	HttpRequest->OnProcessRequestComplete().BindLambda(
+		[&](const FHttpRequestPtr& Req, const FHttpResponsePtr& Res)
+	{
+		Response.IsLoading = false;
+		Request.Callback(Req, Res);
+	});
+
+	HttpRequest->ProcessRequest();
+}
---
+++ Source/ProjectZam/Utils/ApiUtil.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Interfaces/IHttpRequest.h"
+
+struct FApiRequest {
+	FString Path;
+	std::function<void (FHttpRequestPtr Request, FHttpResponsePtr Response)> Callback;
+	UStruct Params;
+	TMap<FString, FString> Header;
+};
+
+struct FApiResponse {
+	bool IsLoading;
+};
+
+class FAPIUtil
+{
+
+public:
+	FAPIUtil();
+	
+	static FAPIUtil MainAPI;
+
+	void GetApi(const FApiRequest& Request, FApiResponse& Response) const;
+	void PostApi(const FApiRequest& Request, FApiResponse& Response) const;
+	
+private:
+	FString MainURL;
+
+	void ParsingData(UStruct Params, FString& Result) const;
+};
